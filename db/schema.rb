6bee9955ac5a007eeb7ca54733c2a4f7ed9d5ab3
Emeric Henon <emeric.henon@gmail.com>
2016-08-11 12:03:50 +0200

    good models et generators init rake ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811092006) do
+ActiveRecord::Schema.define(version: 20160811093201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160811092006) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string   "name"
     t.string   "description"
     t.integer  "cocktail_id"
     t.integer  "ingredient_id"
