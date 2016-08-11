feeb100c114c8e610e283e3802c2a63aed25922c
Emeric Henon <emeric.henon@gmail.com>
2016-08-11 14:55:47 +0200

    new de doses marche

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,16 +4,19 @@ class DosesController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
 
+    @dose = Dose.create(dose_params)
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-
+    params.require(:dose).permit(:description, :ingredient)
   end
 end
