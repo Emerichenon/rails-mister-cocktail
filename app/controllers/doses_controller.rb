6fdd11925019c9cedf240633550f53f73e067272
Emeric Henon <emeric.henon@gmail.com>
2016-08-11 14:31:55 +0200

    doses routes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,19 @@
 class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def dose_params
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Cocktail.destroy_all
 Ingredient.destroy_all
+Dose.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+
+Cocktail.create!(name: "mojito")
+Cocktail.create!(name: "margarita")
+Cocktail.create!(name: "tgv")
+
+Dose.create!(description: "half a fruit", cocktail: Cocktail.all.sample, ingredient: Ingredient.all.sample)
